4a34a6224494e43f5b1da921d3177aadfa54bbca
Mask612 <michael.a4964@gmail.com>
2016-12-12 22:50:53 -0600

    Create info.h

---
+++ info.h
@@ -0,0 +1,12 @@
+#ifndef INFO_H_INCLUDED
+#define INFO_H_INCLUDED
+
+#include <stdio.h>
+
+void idString(char name[], char id[]);
+void commandMessage();
+void objectives();
+void side(FILE *fileScenario);
+void ai(FILE *fileScenario);
+void addNull(char inArray[]);
+#endif // INFO_H_INCLUDED
